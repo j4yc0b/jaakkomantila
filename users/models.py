1adfea4e51f2725c30db14b5ee44089a36d11716
Jaakko <jaakko.mantila@hotmail.com>
2022-07-02 12:23:12 +0200

    something

--- users/models.py
+++ users/models.py
@@ -15,18 +15,18 @@ class Profile(models.Model):
     profile_image = models.ImageField(
         null=True, blank=True, upload_to='profiles/', default='profiles/default_dude.png')
     social_github = models.CharField(max_length=200, null=True, blank=True)
+    social_linkedin = models.CharField(max_length=200, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                             primary_key=True, editable=False)
 
-    skills = models.ManyToManyField('Skill', blank=True)
+
 
     def __str__(self):
         return str(self.user)
 
 
 class Skill(models.Model):
-    # owner = models.ForeignKey(Profile, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
