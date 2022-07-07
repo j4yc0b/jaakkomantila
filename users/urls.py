e877358982e791204e7023c8324d147fc9d8bbd2
Jaakko <jaakko.mantila@hotmail.com>
2022-07-07 15:02:49 +0200

    minor updated

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.profiles, name="profiles"),
     path('profiles/<str:pk>/', views.user_profile, name="user_profile"),
     path('aboutme/', views.aboutme, name="aboutme"),
+    path('aboutme/', views.skills, name="skills"),
 ]
--- users/views.py
+++ users/views.py
@@ -7,24 +7,16 @@ def profiles(request):
     context = {'profiles': profiles}
     return render(request, 'users/profiles.html', context)
     
-
-
 def user_profile(request, pk):
     profile = Profile.objects.get(id=pk)
 
-
 def aboutme(request):
     project_obj = Profile.objects.get(name='Jaakko Mantila')
     context = {'info':project_obj}
     return render(request,'users/aboutme.html', context)
 
-
-def aboutme(request):
-    project_obj = Profile.objects.get(name='Jaakko Mantila')
-    context = {'info':project_obj}
-    return render(request,'users/aboutme.html', context)
-
-def skill(request, pk):
-    skillObj= Skill.objects.all()
-    return render(request,'users/profile.html',{'skills':skillObj})
+def skills(request):
+    skillObj = Skill.objects.all()
+    context = {'skills':skillObj}
+    return render(request,'users/aboutme.html',context)
 
